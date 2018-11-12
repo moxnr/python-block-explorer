161c5402b3994c51e178cb4261801922668ad3b9
Nermannen <max@nerman.it>
2018-11-12 20:55:46 +0800

    List all transactions and outputs of adress implemented

--- p1-Max-Nerman.py
+++ p1-Max-Nerman.py
@@ -91,7 +91,54 @@ def findTransaction():
             output = str(transaction['vout'][index]['value']) + ' BTE till: ' + str(transaction['vout'][index]['scriptPubKey']['addresses'][0])
         print('Output ' + str(index) + ': ' + output) 
 
-# 
+# Find all outputs of address
+def listOutputs():
+    print('Ange adress: ')
+    address = str(input())
+    print('-------------------')
+    print('Söker efter adress: ' + address)
+    # Get the search-span of blocks 
+    totalBlocks = response['result']['blocks']
+    searchBlock = totalBlocks - 2000
+    startBlock = searchBlock
+    searchedBlocks = 0
+    # Iterate through blocks and their transactions to find matching addresses
+    while searchBlock <= totalBlocks:
+        # Print number of searched blocks
+        if searchBlock == startBlock + 500:
+            searchedBlocks += 500
+            startBlock += 500
+            print(searchedBlocks)
+        # Check block for transactions
+        firstPayload = {
+            "method": "getblockhash",
+            "params": [ searchBlock ],
+        }
+        secondResponse = requests.post(url, data=json.dumps(firstPayload), headers=headers).json()
+        secondPayload = {
+            "method": "getblock",
+            "params": [ secondResponse['result']],
+        }
+        thirdResponse = requests.post(url, data=json.dumps(secondPayload), headers=headers).json()
+        transactions = thirdResponse['result']
+        # Iterate transactions to find address
+        for index in range(len(transactions['tx'])):
+            payload = {
+                "method": "getrawtransaction",
+                "params": [ transactions['tx'][index], 1 ]
+            }
+            fourthResponse = requests.post(url, data=json.dumps(payload), headers=headers).json()
+            rawTransaction = fourthResponse['result']
+            for secondIndex in range(len(rawTransaction['vout'])):
+                if rawTransaction['vout'][secondIndex]['scriptPubKey']['type'] == 'nulldata':
+                    break
+                elif rawTransaction['vout'][secondIndex]['scriptPubKey']['addresses'][0] == address: 
+                    output = str(rawTransaction['vout'][secondIndex]['value']) + ' BTE'
+                    print('Block ' + str(searchBlock) + ', Tx: ' + transactions['tx'][index])
+                    print('output: ' + transactions['tx'][secondIndex])
+        # Test adress 1eduGsrvBJcfyTMij2rYXk9viiVV78PNq
+        # Increase to search next block
+        searchBlock += 1 
 
 print('Bitcoin Edu utforskare')
 print('==============================')
